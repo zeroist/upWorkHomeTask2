bfcf16e7f42e3fad96648283b0c05b4e450a943f
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-10-11 17:07:43 +0300

    utilities/searchUtils/doesSearchResultExist method fixed

--- src/test/java/com/upwork/utilities/SearchUtils.java
+++ src/test/java/com/upwork/utilities/SearchUtils.java
@@ -7,20 +7,21 @@ In this class only general utility methods that are NOT related to some specific
 
 public class SearchUtils {
     /**
-     *
      * @param text
      * @return boolean and if it returns false it means there is no search result.
      */
-    public static boolean doesSearchResultExist(String text){
-        boolean result=true;
+    public static boolean doesSearchResultExist(String text) {
+        boolean result = true;
         char[] chars = text.toCharArray();
 
         for (char eachChar : chars) {
-            if(Character.isDigit(eachChar)){
-                if (eachChar==48){
-                    result=false;
+            if (Character.isDigit(eachChar)) {
+                if (eachChar == 48) {
+                    result = false;
                 }
+                break;
             }
+
         }
 
         return result;
