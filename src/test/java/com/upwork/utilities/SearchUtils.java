d317748295fa9c27f0a803ff52715b8ed16c534d
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-10-11 10:25:12 +0300

    biraz intellij tavsiyesi uyguladım

--- src/test/java/com/upwork/utilities/SearchUtils.java
+++ src/test/java/com/upwork/utilities/SearchUtils.java
@@ -6,7 +6,11 @@ In this class only general utility methods that are NOT related to some specific
 
 
 public class SearchUtils {
-
+    /**
+     *
+     * @param text
+     * @return boolean and it returns false it means there is no search result.
+     */
     public static boolean doesSearchResultExist(String text){
         boolean result=true;
         char[] chars = text.toCharArray();
